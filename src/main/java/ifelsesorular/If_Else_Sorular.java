edd532e2ac31accfb3ae0cef64c309afbf3df4c0
Balcihatice <aciceaybike@gmail.com>
2022-10-19 16:25:42 +0300

    ilk degisiklik

--- src/main/java/ifelsesorular/If_Else_Sorular.java
+++ src/main/java/ifelsesorular/If_Else_Sorular.java
@@ -1,8 +1,27 @@
 package ifelsesorular;
 
+import java.util.Scanner;
+
 public class If_Else_Sorular {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Bir sayi giriniz");
+
+        int sayi = scan.nextInt();
+
+        if (sayi >= 0) {
+
+            System.out.println("Negatif değil");
+
+        } else {
+
+            System.out.println("Negatif");
+
+        }
+
+
     }
 
 }
